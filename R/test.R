eadb929c47bb5c5a61e88f66041062faf865693f
thomas-tams-dtu <s204540@dtu.dk>
2023-11-21 21:53:40 +0100

    update: test other stuff

--- R/test.R
+++ R/test.R
@@ -1,28 +1,20 @@
-# Install and load required packages
-install.packages("tidyr")
-library(tidyr)
+library(stringr)
 
-# Create a sample data frame
-data <- data.frame(
-  ID = 1:3,
-  Value1_A = c(10, 20, 30),
-  Value2_A = c(40, 50, 60),
-  Value1_B = c(70, 80, 90),
-  Value2_B = c(100, 110, 120)
-)
+# Example string
+text <- "This is an example (123) with some (456) groups."
 
-# Display the original data frame
-print("Original Data:")
-print(data)
+# Define a regular expression with capturing groups
+pattern <- "\\((\\d+)\\)"
 
-# Pivot longer using two sets of values
-data_long <- data %>%
-  pivot_longer(
-    cols = starts_with("Value"),
-    names_to = c(".value", "Set"),
-    names_sep = "_"
-  )
+# Use str_match to extract the first capturing group
+matches <- str_match(text, pattern)
 
-# Display the resulting long-format data frame
-print("\nData Long:")
-print(data_long)
+matches
+
+# Extract the first capturing group
+if (!is.na(matches[1, 2])) {
+  result <- matches[1, 2]
+  cat("Extracted value:", result, "\n")
+} else {
+  cat("No match found.\n")
+}
